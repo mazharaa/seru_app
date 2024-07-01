ef6de27a63cd4c8b90109b59a6fc7c2bcedf89ab
azhar <mazharaa32@gmail.com>
2024-07-01 23:54:48 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SeruApp());
+}
+
+class SeruApp extends StatelessWidget {
+  const SeruApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
